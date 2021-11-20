b99b3d91d24b57da42cba360572b5004f55fa0af
GerardBurgues <gerard61999@gmail.com>
2021-11-20 17:40:31 +0100

    Parte 3 PCA

--- main.py
+++ main.py
@@ -4,23 +4,32 @@ from sklearn.linear_model import LogisticRegression
 from sklearn import svm, datasets
 from sklearn.model_selection import train_test_split
 import pandas as pd
-# import some data to play with
+from sklearn import preprocessing
 import seaborn as sns
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import f1_score, precision_recall_curve, average_precision_score, roc_curve, auc
-
-
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 
 def dropData(data):
 
-
-
-
-
-
     return data.drop(["Date","Evaporation","Sunshine", "Cloud9am", "Cloud3pm"] , axis=1)
+def OrdinalEncoderFunc(data):
+    """
+    This function is an OrdinalEncoderFunction using sklearn
+    Also Fill Nans with mean values
+    """
+    enc = OrdinalEncoder()
+    enc.fit(data[["Location","WindGustDir","WindDir9am","WindDir3pm","RainToday","RainTomorrow"]])
+    data[["Location","WindGustDir","WindDir9am","WindDir3pm","RainToday","RainTomorrow"]] = \
+        enc.transform(data[["Location","WindGustDir","WindDir9am","WindDir3pm","RainToday","RainTomorrow"]])
 
-def transformData(data):
+    df = pd.DataFrame(data)
+    for i in df.columns:
+            df[i].fillna(value=data[i].mean(), inplace=True)
+    return df
+def OneHotEncoder(data):
     Location  = pd.get_dummies(data["Location"], prefix="loc_")
 
     WindGustDir = pd.get_dummies(data["WindGustDir"], prefix="WGD_")
@@ -43,20 +52,16 @@ def transformData(data):
     for i in df.columns:
             df[i].fillna(value=data[i].mean(), inplace=True)
    # print(data)
-    return data
+    return df
 def SplitData(data):
     dataframe = data.to_numpy()
     X = dataframe[:, 1:-1]
     y = dataframe[:, -1]
     return X,y
-
 def SVMLogistic(X,y):
 
 
     particions = [0.5, 0.7, 0.8]
-    fig, sub = plt.subplots(1, 2, figsize=(16, 6))
-    sub[0].scatter(X[:, 0], y, c=y, cmap=plt.cm.coolwarm, edgecolors='k')
-    sub[1].scatter(X[:, 1], y, c=y, cmap=plt.cm.coolwarm, edgecolors='k')
 
     for part in particions:
         x_t, x_v, y_t, y_v = train_test_split(X, y, train_size=part)
@@ -83,8 +88,12 @@ def SVMLogistic(X,y):
         print("Correct classification SVM      ", part, "% of the data: ", svc.score(x_v, y_v))
 
     ROCFunction(y_v, probs, 3)
+def StandarizeData(data):
 
-
+    scaler = StandardScaler()
+    scaled = scaler.fit_transform(data)
+    scaled = pd.DataFrame(scaled)
+    return scaled
 def NormalizeData(data):
 
     """We normalize data to have the values between 0 and 1 """
@@ -95,8 +104,7 @@ def NormalizeData(data):
     dataNormalize =  pd.DataFrame(dataNormalize)
 
     return dataNormalize
-
-def ROCFunction(y_v,probs, n_classes):
+def ROCFunction(y_v,probs, n_classes=3):
 
 
     # Compute Precision-Recall and plot curve
@@ -105,6 +113,8 @@ def ROCFunction(y_v,probs, n_classes):
     average_precision = {}
     plt.figure()
     for i in range(n_classes):
+        print(y_v)
+        print(probs[:,i])
         precision[i], recall[i], _ = precision_recall_curve(y_v == i, probs[:, i])
         average_precision[i] = average_precision_score(y_v == i, probs[:, i])
 
@@ -130,14 +140,40 @@ def ROCFunction(y_v,probs, n_classes):
         plt.plot(fpr[i], tpr[i], label='ROC curve of class {0} (area = {1:0.2f})' ''.format(i, roc_auc[i]))
     plt.legend()
     plt.show()
+
+def PCAFunc(data):
+    pca  = PCA(n_components=2)
+    pca.fit(data)
+    print(" explained variance ration", pca.explained_variance_ratio_,"\n")
+    print("singular" , pca.singular_values_)
 if __name__ == "__main__":
+
     data = pd.read_csv('weatherAUS.csv')
+    print("Choose what type of Normalization you want: Standard or MinMax : \n")
+    NormType =  input()
+    #print("How Nan affect Data: choose deleting rows (DEL) or NAn = mean value (MEAN) \n")
+    #NanType = input()
+    print("What type of Encoder do you want Ordinal or Hot : \n")
+    Encoder = input()
+
+    #Deleting rows we don't want
     data = dropData(data)
-    data = transformData(data)
-    data = NormalizeData(data)
+    if Encoder == "Hot":
+        data = OneHotEncoder(data)
+    else:
+        #data = OrdinalEncoderFunc(data)
+        print("Not Ready")
+
+    if NormType == "MinMax":
+        data = NormalizeData(data)
+    else:
+        print("StandarizeData() Does not work because does not go 0 to 1")
+        #data = StandarizeData(data)
+
     X,y = SplitData(data)
-    print("esto es X " ,X)
-    print("esto es Y " ,y)
+    print(X)
+    print(y)
+    PCAFunc(data)
     SVMLogistic(X,y)
 
 
